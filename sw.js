1cc600a34582fd3482f7c5582835ba8b1b85007e
lokeshrmitra <lokeshrmitra@gmail.com>
2019-04-02 12:26:22 +0000

    changed notification look

--- sw.js
+++ sw.js
@@ -2,11 +2,23 @@ self.addEventListener('push', function(event) {
     console.log('[Service Worker] Push Received.');
     console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-    const title = 'Push Codelab';
+    const title = 'Uberization';
     const options = {
       body: event.data.text(),
       icon: 'images/icon.png',
-      badge: 'images/badge.png'
+      badge: 'https://png.icons8.com/search/ff0000',
+      actions: [
+        {
+          action: 'accept',
+          title: 'Accept',
+          icon: 'https://png.icons8.com/correct/'
+        },
+        {
+          action: 'decline',
+          title: 'Decline',
+          icon: 'https://png.icons8.com/x/'
+        }
+      ]
     };
 
     event.waitUntil(self.registration.showNotification(title, options));
